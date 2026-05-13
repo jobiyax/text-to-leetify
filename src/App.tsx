c5369378a00fb24d21f7b58c7392a28be402066e
jobiyax <josuebiyayi404@gmail.com>
2026-05-13 16:26:57 +0100

    chore: add react

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+	return (
+		<div>
+			<h1>BaleLive ⚽</h1>
+			<p>
+				Plateforme simple de gestion du football local en République
+				Démocratique du Congo
+			</p>
+		</div>
+	);
+}
