7fb681f2acd54983efdc7e6d2e63a31560fb1545
jobiyax <josuebiyayi404@gmail.com>
2026-05-13 14:10:12 +0100

    feat: generate and run migrations

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,4 +1,5 @@
 import { SQL } from "bun";
+import { drizzle } from "drizzle-orm/bun-sql";
 
 const databaseUrl = process.env.DATABASE_URL;
 
@@ -6,10 +7,14 @@ if (!databaseUrl) {
 	throw new Error("DATABASE_URL est manquant");
 }
 
-export const db = new SQL(databaseUrl, {
+// Driver SQL natif de Bun
+const client = new SQL(databaseUrl, {
 	max: 10, // max 10 connexions à la base de données
 	idleTimeout: 30, // ferme une connexion inactive après 30s
 	connectionTimeout: 10, // attend 10s max pour se connecter
 
 	bigint: true, // utilise les grands nombres (bigint)
 });
+
+// wrapper Drizzle
+export const db = drizzle(client);
