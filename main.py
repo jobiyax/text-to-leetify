219da85dd0053bb0af7144e3a10190fee5bb585a
jobiyax <josuebiyayi404@gmail.com>
2026-08-04 14:04:22 +0100

    feat: strip accents before conversion

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import unicodedata
+
 LEET = str.maketrans("ABEGILOSTZabegilostz", "48361105724836110572")
 
 
 def leetify(text):
+    text = "".join(
+        c for c in unicodedata.normalize("NFD", text) if not unicodedata.combining(c)
+    )
     return text.translate(LEET)
 
 
