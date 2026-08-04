79a71006c77483ee27bdde0b0f42fe082321393a
jobiyax <josuebiyayi404@gmail.com>
2026-08-04 23:41:18 +0100

    chore: add eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
+
+export default defineConfig({
+  files: ["**/*.{js,ts}"],
+  extends: [js.configs.recommended, tseslint.configs.recommended, prettier],
+});
