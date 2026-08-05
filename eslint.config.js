4d988eb46451bc2d9affcb11f6505005d7210f21
jobiyax <josuebiyayi404@gmail.com>
2026-08-06 00:15:33 +0100

    ci: add GitHub Pages deployment workflow

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import tseslint from "typescript-eslint";
 import prettier from "eslint-config-prettier";
 
 export default defineConfig({
+  ignores: ["dist/**", "node_modules/**"],
   files: ["**/*.{js,ts}"],
   extends: [js.configs.recommended, tseslint.configs.recommended, prettier],
 });
